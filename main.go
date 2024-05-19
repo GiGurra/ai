d6980c21319b4e8c1ee3fc9559afe5cad993883d
Johan Kjölhede <johan.kjolhede@gmail.com>
2024-05-19 15:25:35 +0200

    wip

--- main.go
+++ main.go
@@ -3,19 +3,28 @@ package main
 import (
 	"fmt"
 	"github.com/GiGurra/boa/pkg/boa"
+	"github.com/gigurra/ai/providers/openai"
 	"github.com/spf13/cobra"
+	"gopkg.in/yaml.v3"
 	"log/slog"
+	"os"
 )
 
-type Params struct {
+type CliParams struct {
 	Quiet    boa.Required[bool]   `descr:"Quiet mode, requires no user input" short:"q" default:"false"`
 	Provider boa.Required[string] `descr:"AI provider to use" short:"p" default:"openai"`
 	Model    boa.Required[string] `descr:"Model to use" short:"m" default:"gpt-4o"`
 }
 
+type StoredConfig struct {
+	OpenAI openai.OpenAIConfig `yaml:"openai"`
+}
+
+var defaultConfig = StoredConfig{}
+
 func main() {
 
-	p := Params{}
+	p := CliParams{}
 
 	boa.Wrap{
 		Use:   "ai",
@@ -29,8 +38,70 @@ func main() {
 		Long:   `See the README.MD for more information`,
 		Run: func(cmd *cobra.Command, args []string) {
 
-			slog.Info("Starting ai")
+			// check that file exists
+			homeDir, err := os.UserHomeDir()
+			if err != nil {
+				panic(fmt.Errorf("failed to get home dir: %w", err))
+			}
+
 			slog.Info(fmt.Sprintf("Will use provider: %s", p.Provider.Value()))
+			slog.Info("Will load config from " + homeDir + "/.config/gigurra/ai/config.yaml")
+
+			_, err = os.Stat(homeDir + "/.config/gigurra/ai/config.yaml")
+			if err != nil {
+				if os.IsNotExist(err) {
+					slog.Info("No config file found, will create one")
+					// create file
+					yamlBytes, err := yaml.Marshal(defaultConfig)
+					if err != nil {
+						panic(fmt.Errorf("failed to marshal default config: %w", err))
+					}
+					err = os.MkdirAll(homeDir+"/.config/gigurra/ai", 0755)
+					if err != nil {
+						panic(fmt.Errorf("failed to create config dir: %w", err))
+					}
+					err = os.WriteFile(homeDir+"/.config/gigurra/ai/config.yaml", yamlBytes, 0644)
+					if err != nil {
+						panic(fmt.Errorf("failed to write default config file: %w", err))
+					}
+				} else {
+					panic(fmt.Errorf("failed to stat config file: %w", err))
+				}
+			}
+
+			// load config
+			cfg := StoredConfig{}
+			yamlBytes, err := os.ReadFile(homeDir + "/.config/gigurra/ai/config.yaml")
+			if err != nil {
+				panic(fmt.Errorf("failed to read config file: %w", err))
+			}
+
+			err = yaml.Unmarshal(yamlBytes, &cfg)
+			if err != nil {
+				panic(fmt.Errorf("failed to unmarshal config file: %w", err))
+			}
+
+			switch p.Provider.Value() {
+			case "openai":
+				// check that we have the required config
+				if cfg.OpenAI.APIKey == "" {
+					slog.Error("No openai api key found in config file")
+					os.Exit(1)
+				}
+				models, err := openai.ListModels(cfg.OpenAI)
+				if err != nil {
+					slog.Error(fmt.Sprintf("Failed to list models: %v", err))
+					os.Exit(1)
+				}
+				slog.Info(fmt.Sprintf("Found %d models", len(models.Data)))
+				for _, model := range models.Data {
+					slog.Info(fmt.Sprintf(" - %s", model.ID))
+				}
+			default:
+				slog.Error(fmt.Sprintf("Unknown provider: %s", p.Provider.Value()))
+			}
+
+			// load config from <home>/.config/gigurra/ai/config.yaml
 		},
 	}.ToApp()
 }
--- providers/openai/openai.go
+++ providers/openai/openai.go
@@ -1,6 +1,7 @@
 package openai
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"log/slog"
@@ -8,23 +9,35 @@ import (
 )
 
 type OpenAIConfig struct {
-	APIKey       string
-	Organization string
-	Project      string
+	APIKey       string `yaml:"api_key"`
+	Organization string `yaml:"organization"`
+	Project      string `yaml:"project"`
 }
 
 const baseUrl = "https://api.openai.com/v1/"
 
-func ListModels(cfg OpenAIConfig) (string, error) {
+type OpenAIModelListing struct {
+	Object string        `json:"object"` // will be set to "list"
+	Data   []OpenAIModel `json:"data"`
+}
+
+type OpenAIModel struct {
+	ID        string `json:"id"`
+	Object    string `json:"object"` // will be set to "model"
+	CreatedAt int    `json:"created"`
+	OwnedBy   string `json:"owned_by"`
+}
+
+func ListModels(cfg OpenAIConfig) (OpenAIModelListing, error) {
 	url := baseUrl + "models"
 
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		return "", fmt.Errorf(".ListModels(..): failed to create request: %w", err)
+		return OpenAIModelListing{}, fmt.Errorf(".ListModels(..): failed to create request: %w", err)
 	}
 
 	if cfg.APIKey == "" {
-		return "", fmt.Errorf(".ListModels(..): API key is required, but none was provided")
+		return OpenAIModelListing{}, fmt.Errorf(".ListModels(..): API key is required, but none was provided")
 	}
 	req.Header.Set("Authorization", "Bearer "+cfg.APIKey)
 	if cfg.Organization != "" {
@@ -36,7 +49,7 @@ func ListModels(cfg OpenAIConfig) (string, error) {
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		return "", fmt.Errorf(".ListModels(..): failed to send request: %w", err)
+		return OpenAIModelListing{}, fmt.Errorf(".ListModels(..): failed to send request: %w", err)
 	}
 
 	defer func(Body io.ReadCloser) {
@@ -48,8 +61,14 @@ func ListModels(cfg OpenAIConfig) (string, error) {
 
 	bodyBytes, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return "", fmt.Errorf(".ListModels(..): failed to read response: %w", err)
+		return OpenAIModelListing{}, fmt.Errorf(".ListModels(..): failed to read response: %w", err)
 	}
 
-	return string(bodyBytes), nil
+	listing := OpenAIModelListing{}
+	err = json.Unmarshal(bodyBytes, &listing)
+	if err != nil {
+		return OpenAIModelListing{}, fmt.Errorf(".ListModels(..): failed to unmarshal response body: %w", err)
+	}
+
+	return listing, nil
 }
