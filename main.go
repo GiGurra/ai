a5a848eac6399d9d01652680c7b4e3448d8a3da9
Johan Kjölhede <johan.kjolhede@gmail.com>
2024-05-20 21:47:54 +0200

    update time of staate

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ import (
 	"log/slog"
 	"os"
 	"strings"
+	"time"
 )
 
 func main() {
@@ -164,6 +165,8 @@ func main() {
 				SourceType: domain.Assistant,
 				Content:    accum,
 			})
+
+			state.UpdatedAt = time.Now()
 
 			session.StoreSession(state)
 		},
