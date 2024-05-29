677ae1a304888d49dd97554b48afd0ca74d73730
Johan Kjölhede <johan.kjolhede@gmail.com>
2024-05-29 15:01:42 +0200

    print current in listing

--- main.go
+++ main.go
@@ -128,13 +128,22 @@ func sessionsCmd() *cobra.Command {
 		ParamEnrich: config.CliParamEnricher,
 		Run: func(cmd *cobra.Command, args []string) {
 			sessions := session.ListSessions()
+			currentSession := session.GetSessionID(p.Session.GetOrElse(""))
 			if p.Verbose.Value() {
 				for _, s := range sessions {
-					fmt.Printf("%s (i=%d/%d, o=%d/%d, created %v)\n", s.SessionID, s.InputTokens, s.InputTokensAccum, s.OutputTokens, s.OutputTokensAccum, s.CreatedAt.Format("2006-01-02 15:04:05"))
+					currentSessionSuffix := ""
+					if s.SessionID == currentSession {
+						currentSessionSuffix = " [current]"
+					}
+					fmt.Printf("%s (i=%d/%d, o=%d/%d, created %v)%s\n", s.SessionID, s.InputTokens, s.InputTokensAccum, s.OutputTokens, s.OutputTokensAccum, s.CreatedAt.Format("2006-01-02 15:04:05"), currentSessionSuffix)
 				}
 			} else {
 				for _, s := range sessions {
-					fmt.Printf("%s\n", s.SessionID)
+					currentSessionSuffix := ""
+					if s.SessionID == currentSession {
+						currentSessionSuffix = " [current]"
+					}
+					fmt.Printf("%s%s\n", s.SessionID, currentSessionSuffix)
 				}
 			}
 
