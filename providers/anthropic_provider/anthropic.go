f0d7ff728fb6647c3e2ef6b39e5fbf527a3008f9
Johan Kjölhede <johan.kjolhede@gmail.com>
2024-06-23 20:17:47 +0200

    wip on adding support for anthropic api

--- providers/anthropic_provider/anthropic.go
+++ providers/anthropic_provider/anthropic.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/gigurra/ai/domain"
 	"log/slog"
+	"strings"
 )
 
 type Config struct {
@@ -22,9 +23,69 @@ type Message struct {
 	Content string `json:"content"`
 }
 
+/**
+curl https://api.anthropic.com/v1/messages \
+     --header "anthropic-version: 2023-06-01" \
+     --header "content-type: application/json" \
+     --header "x-api-key: $ANTHROPIC_API_KEY" \
+     --data \
+'{
+  "model": "claude-3-5-sonnet-20240620",
+  "messages": [{"role": "user", "content": "Hello"}],
+  "max_tokens": 256,
+  "stream": true
+}'
+*/
+
+// see https://docs.anthropic.com/en/api/messages-streaming#basic-streaming-request
+
+type BasicAskResponse struct {
+	Choices []domain.Choice
+	Usage   domain.Usage
+}
+
+var _ domain.Response = &BasicAskResponse{}
+
+func (r *BasicAskResponse) GetChoices() []domain.Choice {
+	return r.Choices
+}
+
+func (r *BasicAskResponse) GetUsage() domain.Usage {
+	return r.Usage
+}
+
 func (o Provider) BasicAsk(question domain.Question) (domain.Response, error) {
 
-	return nil, fmt.Errorf("not implemented")
+	stream := o.BasicAskStream(question)
+
+	accum := strings.Builder{}
+
+	for chunk := range stream {
+		if chunk.Err != nil {
+			return nil, chunk.Err
+		}
+		if len(chunk.Resp.GetChoices()) == 0 {
+			return nil, fmt.Errorf("expected at least one choice")
+		}
+		accum.WriteString(chunk.Resp.GetChoices()[0].Message.Content)
+	}
+
+	return &BasicAskResponse{
+		Choices: []domain.Choice{
+			{
+				Index: 0,
+				Message: domain.Message{
+					SourceType: domain.Assistant,
+					Content:    accum.String(),
+				},
+			},
+		},
+		Usage: domain.Usage{
+			PromptTokens:     0,
+			CompletionTokens: 0,
+			TotalTokens:      0,
+		},
+	}, nil
 }
 
 func (o Provider) BasicAskStream(question domain.Question) <-chan domain.RespChunk {
