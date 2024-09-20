4b4ffe6c674911d0cd5cf58d974eaa95bb359ac1
Johan Kjölhede <johan.kjolhede@gmail.com>
2024-09-20 22:10:09 +0200

    Add some doc/comments to redundant test

--- providers/anthropic_provider/anthropic_test.go
+++ providers/anthropic_provider/anthropic_test.go
@@ -1,14 +1,22 @@
 package anthropic_provider
 
 import (
+	"fmt"
 	"strings"
 	"testing"
 )
 
 func TestSanitizeLines(t *testing.T) {
 
+	// I should probably remove this test.
+	// I created it just to investigate the strings.Split semantics in go's standard library.
+
 	input := "Hello\nWorld\n"
 	lines := strings.Split(input, "\n")
+
+	for i, line := range lines {
+		fmt.Printf("Line %d: %s\n", i, line)
+	}
 
 	if len(lines) != 3 {
 		t.Errorf("Expected 3 lines, got %d", len(lines))
